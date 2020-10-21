ef86476c2b14ce78ff5f829d8c2d14833b4939aa
Connor <cstreng95@gmail.com>
2020-10-21 20:49:55 +0200

    Updates

---
+++ precache-manifest.b0a7f6ffcde86fc329932c11ec8a146a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba56f12236999462d55e77fb6d21722",
+    "url": "/borrel-beter/index.html"
+  },
+  {
+    "revision": "d84899d150a5c314e1b7",
+    "url": "/borrel-beter/static/css/main.6f780172.chunk.css"
+  },
+  {
+    "revision": "4e9d925b7f51cfd14cdd",
+    "url": "/borrel-beter/static/js/2.7a511f86.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/borrel-beter/static/js/2.7a511f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84899d150a5c314e1b7",
+    "url": "/borrel-beter/static/js/main.1ce26349.chunk.js"
+  },
+  {
+    "revision": "e95234136659b2a88610",
+    "url": "/borrel-beter/static/js/runtime-main.d5f0c9f5.js"
+  },
+  {
+    "revision": "06ce422b738b8bbd9b7d5e28a0c6a0d4",
+    "url": "/borrel-beter/static/media/video.06ce422b.mp4"
+  }
+]);
